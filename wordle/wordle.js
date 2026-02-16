5a101ab569eaa668065badc8bf314a66e0490958
ControllerTaikoFan <taikofan2018@gmail.com>
2026-02-16 12:15:55 -0800

    add refresh functionality, change tile color after generation if set as X

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -28,7 +28,7 @@ function readFile() {
 
 
 
-const url = "https://raw.githubusercontent.com/ControllerTaikoFan/controllertaikofan.github.io/refs/heads/main/wordle/dictionary/dictionary.txt"
+const url = "https://raw.githubusercontent.com/ControllerTaikoFan/controllertaikofan.github.io/refs/heads/main/wordle/dictionary/dictionary.txt";
 fetch(url)
     .then(r => r.text())
     .then(t => {
@@ -44,6 +44,14 @@ let pattern = [
     ["_", "_", "_", "_", "_"]
 ]
 
+let triedWords = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+];
 syncPatternAndGrid();
 
 
@@ -52,7 +60,7 @@ allTiles.forEach(child => {
         const tileRow = rows.indexOf(child.id[0]);
         const tileCol = Number(child.id[1]);
         const tileImage = child.querySelector(".tile-image");
-        switch (pattern[tileRow][tileCol]) {
+        switch (pattern[tileRow][tileCol]) { //  cycle between _, Y, G, and X
             case "_":
                 tileImage.src = "assets/yellow.png";
                 pattern[tileRow][tileCol] = "Y";
@@ -70,13 +78,60 @@ allTiles.forEach(child => {
                 pattern[tileRow][tileCol] = "_";
                 break;
         }
+
+        triedWords[tileRow] = [];
     })
 });
 
+for (let i = 0; i < 6; i++) {
+    const row = rows[i];
+    document.getElementById("refresh-" + row).addEventListener("click", function () {
+        const solutionWord = solutionWordInput.value.trim().toLowerCase();
+
+        let letterCount = new Map();
+        for (const letter of solutionWord) {
+            if (letterCount.has(letter)) {
+                letterCount.set(letter, letterCount.get(letter) + 1);
+            } else {
+                letterCount.set(letter, 1);
+            }
+        }
+        console.log(triedWords[i])
+        let word = check(solutionWord, letterCount, pattern[i], triedWords[i]);
+        if (word == triedWords[i][0]) {
+            triedWords[i] = [word];
+        } else {
+            triedWords[i].push(word);
+        }
+
+
+
+        if (word !== "") {
+            for (let j = 0; j < 5; j++) {
+                document.getElementById("error-" + rows[i]).style.visibility = "hidden";
+                document.getElementById(row + j).querySelector(".tile-text").innerText = word[j].toUpperCase();
+            }
+        } else {
+            document.getElementById("error-" + rows[i]).style.visibility = "visible";
+            for (let j = 0; j < 5; j++) {
+                document.getElementById(rows[i] + j).querySelector(".tile-text").innerText = "";
+            }
+        }
+    });
+}
 
 function determineWord() {
     let solutionPattern = [];
     const solutionWord = solutionWordInput.value.trim().toLowerCase();
+
+    triedWords = [
+        [],
+        [],
+        [],
+        [],
+        [],
+        []
+    ];
 
     let letterCount = new Map();
     for (const letter of solutionWord) {
@@ -87,29 +142,40 @@ function determineWord() {
         }
     }
 
-    for (const line of pattern) {
-        let patternWord = check(solutionWord, letterCount, line);
+    for (let i = 0; i < pattern.length; i++) {
+        const line = pattern[i];
+        let patternWord = check(solutionWord, letterCount, line, triedWords[i]);
         solutionPattern.push(patternWord);
+
+        if (patternWord != "") {
+            triedWords[i].push(patternWord);
+            document.getElementById("refresh-" + rows[i]).style.visibility = "visible";
+        } else {
+            document.getElementById("refresh-" + rows[i]).style.visibility = "hidden";
+        }
     }
 
     displayResults(solutionPattern);
+    syncPatternAndGrid();
 
 }
 
-function check(solution, solutionLetterCount, line) {
+function check(solution, solutionLetterCount, line, forbiddenWords) {
     let found_word = ""
     for (const word of dictionary) {
-
+        if (forbiddenWords.includes(word)) {
+            console.log('aspdokaspjikod')
+            continue;
+        }
         if (word == solution) {
             let solutionValid = true;
-            for (let i = 0; i < 5; i++) {
+            for (let i = 0; i < 5; i++) { // check if the line is all green
                 if (line[i] != "G") {
                     solutionValid = false;
                 }
             }
             if (solutionValid) {
-                found_word = solution;
-                break;
+                return found_word;
             }
             continue;
         }
@@ -124,6 +190,12 @@ function check(solution, solutionLetterCount, line) {
                 case "X":
                     if (!solution.includes(currentLetter)) {
                         valid = false;
+                    } else {
+                        if (solution[i] == currentLetter) { // change the X to either Y or G
+                            line[i] = "G";
+                        } else {
+                            line[i] = "Y";
+                        }
                     }
                     break;
                 case "Y":
@@ -168,6 +240,12 @@ function check(solution, solutionLetterCount, line) {
         }
     }
 
+    if (found_word === "") {
+        console.log("aopksjd")
+        if (forbiddenWords.length > 0) {
+            found_word = forbiddenWords[0];
+        }
+    }
 
     return found_word;
 }
@@ -202,7 +280,7 @@ function syncPatternAndGrid() {
                     tileImage.src = "assets/yellow.png";
                     break;
                 case "G":
-                    tileImage.src = "assets.green.png";
+                    tileImage.src = "assets/green.png";
                     break;
                 case "X":
                     tileImage.src = "assets/any.png";
